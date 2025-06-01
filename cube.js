b71dc2e1850ffd74b1649321e3eb5af570a9e68b
GattoFofo <gu12peruchi@hotmail.com>
2025-05-31 23:31:02 -0300

    Criado menu e canvas exemplo

--- cube.js
+++ cube.js
@@ -1,6 +1,7 @@
-WebAssembly.instantiateStreaming(fetch("main.wasm")).then(
-  (obj) => {
-    // Do something with the results!
-	console.log(obj.instance.exports.main());
-  },
-);
+WebAssembly.instantiateStreaming(fetch("main.wasm")).then((obj) => {console.log(obj.instance.exports.main());});
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
